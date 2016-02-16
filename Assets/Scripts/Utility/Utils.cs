38bf0affc54d31f9ad93cfa301c3425a82f2ad89
Usama <usama@mindstormstudios.com>
2016-02-16 15:23:56 +0500

    Add amazonPlatform check.

--- Assets/Scripts/Utility/Utils.cs
+++ Assets/Scripts/Utility/Utils.cs
@@ -1,14 +1,20 @@
-﻿// #define AMAZON
+﻿#define UNITY_AMAZON
 using UnityEngine;
 
 namespace MS.Utility
 {
     public static class Utils
     {
-        public static bool IsAmazonPlatform = false; 
-        #if (AMAZON)        
-         	   IsAmazonPlatform = true;                
-        #endif
+        public static bool IsAmazonPlatform
+        {
+            get
+            {
+                #if UNITY_AMAZON
+                return true;
+                #endif
+                return false;
+            }
+        }                                         	                           
         public static bool IsNetWorkAvailable()
         {
             return (Application.internetReachability != NetworkReachability.NotReachable);
