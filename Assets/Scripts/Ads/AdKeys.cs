d77a634ef6ea11cb393ec22531f8c8f557c0b635
Usama <usama@mindstormstudios.com>
2016-02-16 15:27:50 +0500

    Change const strings to static
    Strings changed becasue they can be edited later from web or file.

--- Assets/Scripts/Ads/AdKeys.cs
+++ Assets/Scripts/Ads/AdKeys.cs
@@ -4,13 +4,13 @@ namespace MS.Ads
     public static class AdKeys 
     {        
         #if UNITY_ANDROID
-        public const string FyberAppId = "40473";
-        public const string FyberUserId = "";
-        public const string FyberSecurityToken = "25c96bd97e2c730375c61a16e324a86a";
+        public static string FyberAppId = "40473";
+        public static string FyberUserId = "";
+        public static string FyberSecurityToken = "25c96bd97e2c730375c61a16e324a86a";
         #elif UNITY_IOS        
-        public const string FyberAppId = "40473";
-        public const string FyberUserId = "";
-        public const string FyberSecurityToken = "25c96bd97e2c730375c61a16e324a86a";
+        public static string FyberAppId = "40473";
+        public static string FyberUserId = "";
+        public static string FyberSecurityToken = "25c96bd97e2c730375c61a16e324a86a";
         #elif UNITY_WINRT_10_0 || UNITY_WINRT_8_1
         #elif UNITY_AMAZON
         #endif
